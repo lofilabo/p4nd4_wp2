4513d7aea2cf98270fa6708e74212a37170456f7
Lofilabo <github@lofilabo.com>
2020-01-13 05:04:41 +0900

    index and page templates all of them

---
+++ page-templates/index.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: Index */ ?>
+<?php
+get_header();
+
+$pages = get_pages(); 
+
+//var_dump( $pages );
+
+   foreach ($pages as $page) {
+
+       $permalink = get_permalink( $page->ID );
+       echo ( "<a href='".  $permalink  ."'>"  );
+       echo $page->post_title;
+       echo ( "</a>" );
+       
+       echo( "<br/>" );
+
+   }
+
+?>
+</h6>
+<?php 
+get_footer();
+?>
---
+++ page-templates/page.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Page */ ?>
+<?php
+
+get_header();
+
+ if(have_posts()) : ?>
+    <?php while(have_posts())  : the_post(); ?>
+      <h2><?php the_title(); ?></h2>                        
+      <?php the_content(); ?>          
+      <? // php comments_template( '', true ); ?> 
+    <?php endwhile; ?>                   
+      <?php else : ?>                       
+        <h3><?php _e('404 Error&#58; Not Found'); ?></h3>
+<?php endif; ?> 
+
+
+</h6>
+<?php 
+get_footer();
+?>
